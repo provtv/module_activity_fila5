079136eb5153636b5ca04175edf204dec0343e38
marco76tv <marco.sottana@gmail.com>
2026-07-28 19:15:08 +0200

    .

--- .vscode/.php-cs-fixer.php
+++ .vscode/.php-cs-fixer.php
@@ -8,7 +8,7 @@ $finder = PhpCsFixer\Finder::create()
     ->name('*.php')
     ->notName('*.blade.php')
     ->ignoreDotFiles(true)
-    ->ignoreVCS(true)
+    ->ignoreVCS(true);
 
 $config = new PhpCsFixer\Config();
 
@@ -35,6 +35,6 @@ $config
         'class_definition' => true,
         'elseif' => true,
     ])
-    ->setFinder($finder)
+    ->setFinder($finder);
 
 return $config;
